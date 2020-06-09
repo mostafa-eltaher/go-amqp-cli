d6f34da8bc7e8acae52bd0067cef5704cd9642bf
mostafa-eltaher <mostafa.mahmoud.eltaher@gmail.com>
2020-06-09 02:02:10 +0100

    WIP: first working version only sending or receiving

---
+++ Makefile
@@ -0,0 +1,31 @@
+SNAPSHOT_NUMBER :=$(shell date +'%y%m%d')
+VERSION_NUMBER=0.1
+VERSION=$(VERSION_NUMBER)-b$(SNAPSHOT_NUMBER)
+
+BINARY = bin
+SRC = cmd/amqp-cli
+APP = amqp-cli
+
+.DEFAULT_GOAL := all
+
+.PHONY: all
+all: clean build
+
+.PHONY: pre-compile
+pre-compile:
+	@mkdir -p ${BINARY}
+
+compile: pre-compile
+	@echo "Compiling for every OS and Platform"
+	GOOS=linux GOARCH=amd64 go build -o ${BINARY}/${APP}-v${VERSION}-linux-amd64 ./${SRC}
+	GOOS=windows GOARCH=amd64 go build -o ${BINARY}/${APP}-v${VERSION}-windows-amd64 ./${SRC}
+
+build: compile
+
+clean:
+	@if [ -d ${BINARY} ] ; then rm -rf ${BINARY}/* ; fi
+
+.PHONY: sync
+sync:
+	scp ${BINARY}/*-linux-amd64 localvm:/home/mostafa/projects/enmasse/enmasse-0.31.2/go-amqp-cli
+	scp cmd/amqp-cli/*.yaml localvm:/home/mostafa/projects/enmasse/enmasse-0.31.2/go-amqp-cli
--- cmd/amqp-cli/main.go
+++ cmd/amqp-cli/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"time"
 
+	"github.com/Azure/go-amqp"
 	"gopkg.in/yaml.v2"
 )
 
@@ -13,6 +19,7 @@ import (
 type YamlConfig struct {
 	Spec struct {
 		Connection struct {
+			Container      string `yaml:"container"`
 			Authentication struct {
 				Type     string `yaml:"type"`
 				Username string `yaml:"username"`
@@ -24,7 +31,7 @@ type YamlConfig struct {
 				Role          string `yaml:"role"`
 				Source        string `yaml:"source"`
 				Target        string `yaml:"target"`
-				InitialCredit int    `yaml:"initialCredit"`
+				InitialCredit uint32 `yaml:"initialCredit"`
 			} `yaml:"links"`
 		} `yaml:"sessions"`
 	} `yaml:"spec"`
@@ -32,11 +39,104 @@ type YamlConfig struct {
 
 func main() {
 	fmt.Println("Parsing YAML file")
+	reader := bufio.NewReader(os.Stdin)
 
 	yamlConfig, err := parseConf()
 
 	if err != nil {
 		fmt.Printf("Error parsing YAML file: %s\n", err)
+		panic(err)
+	}
+	container := yamlConfig.Spec.Connection.Container
+	username := yamlConfig.Spec.Connection.Authentication.Username
+	password := yamlConfig.Spec.Connection.Authentication.Password
+
+	log.Printf("Dialing AMQP server: %s", container)
+	client, err := amqp.Dial(container,
+		amqp.ConnSASLPlain(username, password),
+	)
+	if err != nil {
+		log.Fatal("Dialing AMQP server:", err)
+	}
+	defer client.Close()
+	// Open a session
+	for _, sessionConfig := range yamlConfig.Spec.Sessions {
+		session, err := client.NewSession()
+		if err != nil {
+			log.Fatal("Creating AMQP session:", err)
+		}
+
+		ctx := context.Background()
+		defer session.Close(ctx)
+		for _, linkConfig := range sessionConfig.Links {
+
+			if linkConfig.Role == "sender" {
+				// Send a message
+				{
+					// Create a sender
+					sender, err := session.NewSender(
+						amqp.LinkTargetAddress(linkConfig.Target),
+					)
+					if err != nil {
+						log.Fatal("Creating sender link:", err)
+					}
+
+					//ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+					//ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+
+					// Send message
+					err = sender.Send(ctx, amqp.NewMessage([]byte("Hello!")))
+					if err != nil {
+						log.Fatal("Sending message:", err)
+					}
+
+					func() {
+						for {
+							fmt.Print("-> ")
+							text, _ := reader.ReadString('\n')
+							fmt.Printf("-> SENDING ...%s", text)
+							err = sender.Send(ctx, amqp.NewMessage([]byte(text)))
+							if err != nil {
+								log.Fatal("Sending message:", err)
+							}
+						}
+					}()
+					//defer sender.Close(ctx)
+					//defer cancel()
+				}
+			} else if linkConfig.Role == "receiver" {
+				// Continuously read messages
+				{
+					// Create a receiver
+					receiver, err := session.NewReceiver(
+						amqp.LinkSourceAddress(linkConfig.Source),
+						amqp.LinkCredit(linkConfig.InitialCredit),
+					)
+					if err != nil {
+						log.Fatal("Creating receiver link:", err)
+					}
+					defer func() {
+						ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+						receiver.Close(ctx)
+						cancel()
+					}()
+					func() {
+						for {
+							// Receive next message
+							msg, err := receiver.Receive(ctx)
+							if err != nil {
+								log.Fatal("Reading message from AMQP:", err)
+							}
+
+							// Accept message
+							msg.Accept()
+
+							fmt.Printf("Message received: %s\n", msg.GetData())
+						}
+					}()
+				}
+			}
+		}
 	}
 
 	fmt.Printf("Result: %v\n", yamlConfig)

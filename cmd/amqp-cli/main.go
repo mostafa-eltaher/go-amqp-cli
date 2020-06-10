547961a0f06b86b6553869e5234c83f072ecd751
mostafa-eltaher <mostafa.mahmoud.eltaher@gmail.com>
2020-06-10 04:16:01 +0100

    Use WaitingGroup in main function

--- cmd/amqp-cli/main.go
+++ cmd/amqp-cli/main.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/Azure/go-amqp"
@@ -27,17 +28,21 @@ type YamlConfig struct {
 			} `yaml:"authentication"`
 		} `yaml:"connection"`
 		Sessions []struct {
-			Links []struct {
-				Role          string `yaml:"role"`
-				Source        string `yaml:"source"`
-				Target        string `yaml:"target"`
-				InitialCredit uint32 `yaml:"initialCredit"`
-			} `yaml:"links"`
+			Links []LinkConfig `yaml:"links"`
 		} `yaml:"sessions"`
 	} `yaml:"spec"`
 }
 
+// LinkConfig is exported
+type LinkConfig struct {
+	Role          string `yaml:"role"`
+	Source        string `yaml:"source"`
+	Target        string `yaml:"target"`
+	InitialCredit uint32 `yaml:"initialCredit"`
+}
+
 func main() {
+	var wg sync.WaitGroup
 	fmt.Println("Parsing YAML file")
 	reader := bufio.NewReader(os.Stdin)
 
@@ -64,82 +69,23 @@ func main() {
 		session, err := client.NewSession()
 		if err != nil {
 			log.Fatal("Creating AMQP session:", err)
+			return
 		}
 
 		ctx := context.Background()
 		defer session.Close(ctx)
 		for _, linkConfig := range sessionConfig.Links {
-
 			if linkConfig.Role == "sender" {
-				// Send a message
-				{
-					// Create a sender
-					sender, err := session.NewSender(
-						amqp.LinkTargetAddress(linkConfig.Target),
-					)
-					if err != nil {
-						log.Fatal("Creating sender link:", err)
-					}
-
-					//ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-					//ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-
-					// Send message
-					err = sender.Send(ctx, amqp.NewMessage([]byte("Hello!")))
-					if err != nil {
-						log.Fatal("Sending message:", err)
-					}
-
-					func() {
-						for {
-							fmt.Print("-> ")
-							text, _ := reader.ReadString('\n')
-							fmt.Printf("-> SENDING ...%s", text)
-							err = sender.Send(ctx, amqp.NewMessage([]byte(text)))
-							if err != nil {
-								log.Fatal("Sending message:", err)
-							}
-						}
-					}()
-					//defer sender.Close(ctx)
-					//defer cancel()
-				}
+				wg.Add(1)
+				go createSenderLink(ctx, session, linkConfig, reader, &wg)
 			} else if linkConfig.Role == "receiver" {
-				// Continuously read messages
-				{
-					// Create a receiver
-					receiver, err := session.NewReceiver(
-						amqp.LinkSourceAddress(linkConfig.Source),
-						amqp.LinkCredit(linkConfig.InitialCredit),
-					)
-					if err != nil {
-						log.Fatal("Creating receiver link:", err)
-					}
-					defer func() {
-						ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
-						receiver.Close(ctx)
-						cancel()
-					}()
-					func() {
-						for {
-							// Receive next message
-							msg, err := receiver.Receive(ctx)
-							if err != nil {
-								log.Fatal("Reading message from AMQP:", err)
-							}
-
-							// Accept message
-							msg.Accept()
-
-							fmt.Printf("Message received: %s\n", msg.GetData())
-						}
-					}()
-				}
+				wg.Add(1)
+				go createReceiverLink(ctx, session, linkConfig, &wg)
 			}
 		}
 	}
-
-	fmt.Printf("Result: %v\n", yamlConfig)
+	wg.Wait()
+	//fmt.Printf("Result: %v\n", yamlConfig)
 }
 
 func parseConf() (*YamlConfig, error) {
@@ -162,4 +108,66 @@ func parseConf() (*YamlConfig, error) {
 	err = yaml.Unmarshal(yamlFile, &yamlConfig)
 
 	return &yamlConfig, err
+}
+
+func createSenderLink(ctx context.Context, session *amqp.Session, linkConfig LinkConfig, reader *bufio.Reader, wg *sync.WaitGroup) {
+	// Create a sender
+	defer wg.Done()
+	sender, err := session.NewSender(
+		amqp.LinkTargetAddress(linkConfig.Target),
+	)
+	if err != nil {
+		log.Fatal("Creating sender link:", err)
+		return
+	}
+	//ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+
+	// Send message
+	err = sender.Send(ctx, amqp.NewMessage([]byte("Hello!")))
+	if err != nil {
+		log.Fatal("Sending message:", err)
+	}
+	defer sender.Close(ctx)
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		fmt.Printf("-> SENDING ...%s", text)
+		err = sender.Send(ctx, amqp.NewMessage([]byte(text)))
+		if err != nil {
+			log.Fatal("Sending message:", err)
+		}
+	}
+	//defer cancel()
+}
+
+func createReceiverLink(ctx context.Context, session *amqp.Session, linkConfig LinkConfig, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Create a receiver
+	receiver, err := session.NewReceiver(
+		amqp.LinkSourceAddress(linkConfig.Source),
+		amqp.LinkCredit(linkConfig.InitialCredit),
+	)
+	if err != nil {
+		log.Fatal("Creating receiver link:", err)
+		return
+	}
+	defer func() {
+		ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+		receiver.Close(ctx)
+		cancel()
+	}()
+
+	for {
+		// Receive next message
+		msg, err := receiver.Receive(ctx)
+		if err != nil {
+			log.Fatal("Reading message from AMQP:", err)
+		}
+
+		// Accept message
+		msg.Accept()
+
+		fmt.Printf("Message received: %s\n", msg.GetData())
+	}
 }
